d96d07f8b0915f7053fd80fe2ae27649b306bebe
VeBr <jompawars@gmail.com>
2017-01-27 16:26:28 +0100

    Small changes

--- WindowsFormsApplication3/Shooter.Designer.cs
+++ WindowsFormsApplication3/Shooter.Designer.cs
@@ -46,7 +46,7 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(86, 438);
+            this.pictureBox1.Location = new System.Drawing.Point(1058, 438);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(19, 95);
             this.pictureBox1.TabIndex = 0;

29e2f88c71ac6003440242574119162e1ca34ae5
VeBr <jompawars@gmail.com>
2017-01-30 21:12:55 +0100

    Fixed uBoat speeds so that the increment isn't too fast. Still need highscore files. Already added score, so all I need to do is print Score to a seperate file, and load them to show.

--- WindowsFormsApplication3/Shooter.Designer.cs
+++ WindowsFormsApplication3/Shooter.Designer.cs
@@ -30,28 +30,34 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Torpedo = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Uboat = new System.Windows.Forms.PictureBox();
+            this.CurrentSpeed = new System.Windows.Forms.Label();
+            this.IsClicked = new System.Windows.Forms.Label();
+            this.TorpXY = new System.Windows.Forms.Label();
+            this.UboatXY = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.AmmoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Torpedo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Uboat)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // Torpedo
             // 
-            this.pictureBox1.AccessibleDescription = "Torpedo";
-            this.pictureBox1.AccessibleName = "Torpedo";
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(1058, 438);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(19, 95);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Visible = false;
+            this.Torpedo.AccessibleDescription = "Torpedo";
+            this.Torpedo.AccessibleName = "Torpedo";
+            this.Torpedo.BackColor = System.Drawing.Color.Transparent;
+            this.Torpedo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Torpedo.BackgroundImage")));
+            this.Torpedo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Torpedo.Location = new System.Drawing.Point(1058, 438);
+            this.Torpedo.Name = "Torpedo";
+            this.Torpedo.Size = new System.Drawing.Size(19, 95);
+            this.Torpedo.TabIndex = 0;
+            this.Torpedo.TabStop = false;
+            this.Torpedo.Visible = false;
             // 
             // timer1
             // 
@@ -72,19 +78,75 @@
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
-            // pictureBox3
+            // Uboat
             // 
-            this.pictureBox3.AccessibleDescription = "Torpedo";
-            this.pictureBox3.AccessibleName = "Torpedo";
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(866, 125);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(149, 67);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Visible = false;
+            this.Uboat.AccessibleDescription = "Uboat object";
+            this.Uboat.AccessibleName = "Uboat";
+            this.Uboat.BackColor = System.Drawing.Color.Transparent;
+            this.Uboat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Uboat.BackgroundImage")));
+            this.Uboat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Uboat.Location = new System.Drawing.Point(866, 125);
+            this.Uboat.Name = "Uboat";
+            this.Uboat.Size = new System.Drawing.Size(149, 67);
+            this.Uboat.TabIndex = 2;
+            this.Uboat.TabStop = false;
+            this.Uboat.Visible = false;
+            // 
+            // CurrentSpeed
+            // 
+            this.CurrentSpeed.AutoSize = true;
+            this.CurrentSpeed.Location = new System.Drawing.Point(3, 520);
+            this.CurrentSpeed.Name = "CurrentSpeed";
+            this.CurrentSpeed.Size = new System.Drawing.Size(35, 13);
+            this.CurrentSpeed.TabIndex = 3;
+            this.CurrentSpeed.Text = "label1";
+            this.CurrentSpeed.Visible = false;
+            // 
+            // IsClicked
+            // 
+            this.IsClicked.AutoSize = true;
+            this.IsClicked.Location = new System.Drawing.Point(3, 537);
+            this.IsClicked.Name = "IsClicked";
+            this.IsClicked.Size = new System.Drawing.Size(35, 13);
+            this.IsClicked.TabIndex = 4;
+            this.IsClicked.Text = "label1";
+            this.IsClicked.Visible = false;
+            // 
+            // TorpXY
+            // 
+            this.TorpXY.AutoSize = true;
+            this.TorpXY.Location = new System.Drawing.Point(1212, 536);
+            this.TorpXY.Name = "TorpXY";
+            this.TorpXY.Size = new System.Drawing.Size(35, 13);
+            this.TorpXY.TabIndex = 5;
+            this.TorpXY.Text = "label1";
+            // 
+            // UboatXY
+            // 
+            this.UboatXY.AutoSize = true;
+            this.UboatXY.Location = new System.Drawing.Point(1212, 520);
+            this.UboatXY.Name = "UboatXY";
+            this.UboatXY.Size = new System.Drawing.Size(35, 13);
+            this.UboatXY.TabIndex = 6;
+            this.UboatXY.Text = "label1";
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Location = new System.Drawing.Point(0, 17);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(35, 13);
+            this.ScoreLabel.TabIndex = 7;
+            this.ScoreLabel.Text = "label1";
+            // 
+            // AmmoLabel
+            // 
+            this.AmmoLabel.AutoSize = true;
+            this.AmmoLabel.Location = new System.Drawing.Point(0, 34);
+            this.AmmoLabel.Name = "AmmoLabel";
+            this.AmmoLabel.Size = new System.Drawing.Size(35, 13);
+            this.AmmoLabel.TabIndex = 8;
+            this.AmmoLabel.Text = "label1";
             // 
             // Form1
             // 
@@ -93,26 +155,39 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1261, 627);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.AmmoLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.UboatXY);
+            this.Controls.Add(this.TorpXY);
+            this.Controls.Add(this.IsClicked);
+            this.Controls.Add(this.CurrentSpeed);
+            this.Controls.Add(this.Uboat);
+            this.Controls.Add(this.Torpedo);
             this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Click += new System.EventHandler(this.Form1_Click);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Torpedo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Uboat)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Torpedo;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox Uboat;
+        private System.Windows.Forms.Label CurrentSpeed;
+        private System.Windows.Forms.Label IsClicked;
+        private System.Windows.Forms.Label TorpXY;
+        private System.Windows.Forms.Label UboatXY;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label AmmoLabel;
     }
 }
 
--- WindowsFormsApplication3/Shooter.cs
+++ WindowsFormsApplication3/Shooter.cs
@@ -15,27 +15,34 @@ namespace WindowsFormsApplication3
     {
         public Form1()
         {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
             InitializeComponent();
-            //Enemy enemyClass = new Enemy();
             pictureBox2.Bounds = ClientRectangle;
             timer1.Enabled = true;
         }
 
-        int startPosX, startPosY, curPosX, curPosY, torpSpeed, uPosX, uPosY, uSpeed;
+        int startPosX, startPosY, curPosX, curPosY, torpSpeed, uPosX, uPosY, uSpeed, ammunition = 2, score, UMinSpeed = 1, UMaxSpeed = 11, diffLevel, lossCount;
         Point startPos, relativePoint;
         private int timerCounter;
 
-        public bool clicked = false;
+        private bool clicked = false;
         private bool impact;
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            if (pictureBox1.Location.Y <= ClientRectangle.Height - 1 && clicked == false)
+            if ((impact == true || clicked == false) && ammunition > 0)
             {
                 setTorpedoVariables();
+                ammunition--;
                 clicked = true;
+                diffLevel++;
+                if (diffLevel % 4 == 0)
+                {
+                    UMinSpeed++;
+                    UMaxSpeed++;
+                }
             }
-            Console.WriteLine("Not a valid click");
         }
 
 
@@ -52,7 +59,7 @@ namespace WindowsFormsApplication3
         private void setTorpedoVariables()
         {
             relativePoint = PointToClient(Cursor.Position);
-            torpSpeed = 10;
+            torpSpeed = 30;
             startPosX = ClientRectangle.Width / 2;
             startPosY = ClientRectangle.Height;
             curPosX = startPosX;
@@ -60,7 +67,7 @@ namespace WindowsFormsApplication3
             startPos = new Point(startPosX, startPosY);
             clicked = true;
 
-            pictureBox1.Location = startPos;
+            Torpedo.Location = startPos;
             Console.WriteLine(startPos);
         }
 
@@ -69,41 +76,88 @@ namespace WindowsFormsApplication3
             EnemyControl();
             MissileControl();
             CollisionControl();
+            LabelUpdates();
+            if (lossCondition())
+            {
+                Console.WriteLine("You lost! " + ammunition + " ammo, " + score + " score");
+            }
+        }
+
+        private bool lossCondition()
+        {
+            while (ammunition == 0)
+            {
+                lossCount++;
+                if (lossCount == 50)
+                {
+                    return true;
+                }
+            }
+            if (ammunition > 0)
+            {
+                lossCount = 0;
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void LabelUpdates()
+        {
+            if (pictureBox2.Bounds != ClientRectangle)
+            {
+                pictureBox2.Bounds = ClientRectangle;
+            }
+            IsClicked.Text = clicked.ToString();
+            TorpXY.Text = curPosX + ", " + curPosY;
+            UboatXY.Text = Uboat.Location.X + ", " + Uboat.Location.Y;
+            ScoreLabel.Text = "Score:" + score;
+            AmmoLabel.Text = "Ammo: " + ammunition;
             timerCounter++;
         }
 
         private void CollisionControl()
         {
-            impact = pictureBox1.Bounds.IntersectsWith(pictureBox3.Bounds);
-            if (impact)
+            if (Torpedo.Bounds.IntersectsWith(Uboat.Bounds))
             {
-                ResetAllComp();
+                score += uSpeed;
+                ammunition += 1;
+                Uboat.Visible = false;
+                MissileReset();
             }
         }
 
         private void ResetAllComp()
         {
             Console.WriteLine("Explosion.");
+            if (impact)
+            {
+                Uboat.Visible = false;
+            }
             MissileReset();
-            clicked = false;
         }
 
         private void MissileControl()
         {
             if (clicked == true)
             {
-                Console.WriteLine("Clicked.");
                 MoveMissile();
+            }
+            else if (clicked == false)
+            {
+                MissileReset();
             }
         }
 
         private void EnemyControl()
         {
-            if (pictureBox3.Visible == false)
+            if (Uboat.Visible == false)
             {
                 CreateEnemy();
             }
-            if (pictureBox3.Visible == true)
+            if (Uboat.Visible == true)
             {
                 MoveEnemy();
             }
@@ -112,35 +166,37 @@ namespace WindowsFormsApplication3
         private void MoveEnemy()
         {
             uPosX += uSpeed;
-            pictureBox3.Location = new Point(uPosX, uPosY);
+            Uboat.Location = new Point(uPosX, uPosY);
             if (uPosX >= ClientRectangle.Width)
             {
-                pictureBox3.Visible = false;
+                Uboat.Visible = false;
             }
         }
 
         private void CreateEnemy()
         {
-            if (pictureBox3.Visible == false)
+            if (Uboat.Visible == false)
             {
                 Random r = new Random();
-                uPosX = -pictureBox3.Width;
-                uSpeed = 1 + r.Next(10); // Must be a rand
-                uPosY = 10 + r.Next(70); // Must be a rand
-                pictureBox3.Visible = true;
-                pictureBox3.Location = new Point(uPosX, uPosY);
+                uPosX = -Uboat.Width;
+                uSpeed = r.Next(UMinSpeed, UMaxSpeed); // Must be a rand
+                CurrentSpeed.Text = uSpeed.ToString();
+                uPosY = 10 + r.Next(10, 150); // Must be a rand
+                Uboat.Visible = true;
+                Uboat.Location = new Point(uPosX, uPosY);
             }
-            
+
         }
+
 
         private void MoveMissile()
         {
             if (clicked == true)
             {
-                pictureBox1.Visible = true;
-                if (pictureBox1.Location.Y >= 10)
+                Torpedo.Visible = true;
+                if (Torpedo.Location.Y >= 10)
                 {
-                    pictureBox1.Location = new Point(relativePoint.X, curPosY -= torpSpeed);
+                    Torpedo.Location = new Point(relativePoint.X, curPosY -= torpSpeed);
                 }
                 else
                 {
@@ -151,11 +207,13 @@ namespace WindowsFormsApplication3
 
         private void MissileReset()
         {
-            pictureBox1.Visible = false;
-            pictureBox1.Location = new Point(startPosX, startPosY);
+            Torpedo.Visible = false;
+            clicked = false;
+            Torpedo.Location = startPos;
         }
     }
 }
+
 /*
  Buglist:
  
@@ -164,11 +222,21 @@ namespace WindowsFormsApplication3
             F.Pictures
             F.Moving enemies
             F.Random speeds
+            Change the speed increase
+
             F.Random heights
             Random quantities
+        Boss invader
+            Invader HP + regular's hp
+    
+    Highscores
+        Save to .txt
+        Show highscores
+
+    Add loss/game over condition
             
     Missiles
-        Missiles no longer work as they should
+        F.Missileno longer work as they should
             
     F.Multiple projectiles:
         F.Can launch several missiles by clicking several times.
